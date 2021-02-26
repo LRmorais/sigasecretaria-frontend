eb3a7e112a4b46eeb13d78529e092410cff907bc
Lucas Morais <lucas.morais23ti@gmail.com>
2021-02-25 21:36:53 -0300

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import signIn from '../pages/signin';
+import signIn from '../pages/login';
 import signUp from '../pages/signup';
 
 const Routes: React.FC = () => (
